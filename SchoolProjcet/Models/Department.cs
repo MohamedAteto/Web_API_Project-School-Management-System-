e0143d4c64ce9a2eb2e3f2a6ad71612807c3723a
Dina.ElSayed <Dina.ElSayed@SS-YOUSSEFGOMAA>
2026-09-17 08:27:38 +0300

    Add project files.

---
+++ SchoolProjcet/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<Teacher>Teachers { get; set; } 
+    }
+}
